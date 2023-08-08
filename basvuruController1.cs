3a8f3567b887929ea93f68b48edbb7b9b2a7e2bb
İLAYDA UDUN <81554302+udunilayda@users.noreply.github.com>
2023-08-08 13:45:00 +0300

    Add files via upload

---
+++ basvuruController1.cs
@@ -0,0 +1,20 @@
+﻿using Hayvan_Barınak.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hayvan_Barınak.Controllers
+{
+    public class basvuruController1 : Controller
+    {
+        public IActionResult BasvuruYap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> BasvuruYap(basvuru k, string ReturnUrl)
+        {
+
+
+            return View();
+        }
+    }
+}
